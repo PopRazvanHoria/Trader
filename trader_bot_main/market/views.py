dcd03af071d26fbbed6dbfe2f9ed10dae26fc93d
Razvan <PopRazvanHoria>
2024-08-07 19:17:09 +0300

    renamed explore to market

--- trader_bot_main/market/views.py
+++ trader_bot_main/market/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def crypto_dashboard(request):
-    return render(request, 'explore/crypto_dashboard.html')
+    return render(request, 'crypto_dashboard.html')
--- trader_bot_main/trader_bot_main/asgi.py
+++ trader_bot_main/trader_bot_main/asgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from explore.routing import websocket_urlpatterns
+from market.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trader_bot_main.settings')
 
