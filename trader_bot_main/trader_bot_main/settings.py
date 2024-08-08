76b7dd8aaaa0032e3391532557958b570076077c
Razvan <PopRazvanHoria>
2024-08-08 18:07:49 +0300

    tring to solve asgi connection

--- trader_bot_main/trader_bot_main/settings.py
+++ trader_bot_main/trader_bot_main/settings.py
@@ -125,3 +125,27 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Set the ASGI application for Django Channels
+ASGI_APPLICATION = 'trader_bot_main.asgi.application'
+
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'console': {
+#             'class': 'logging.StreamHandler',
+#         },
+#     },
+#     'loggers': {
+#         'django': {
+#             'handlers': ['console'],
+#             'level': 'DEBUG',
+#         },
+#         'explore': {
+#             'handlers': ['console'],
+#             'level': 'DEBUG',
+#             'propagate': True,
+#         },
+#     },
+# }
